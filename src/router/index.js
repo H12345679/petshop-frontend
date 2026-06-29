f754eace1911cbe8f97f8876211c6918b922f6ba
Hj <85759350@qq.com>
2026-06-29 10:41:15 +0800

    fix: correct UserCenter.vue import path in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/user/HomeView.vue'
+import HomeView from '../views/HomeView.vue'
 import { getStore } from '../libs/storage.js'
 
 Vue.use(VueRouter)
@@ -34,7 +34,7 @@ const routes = [
   {
     path: '/user/center',
     name: 'userCenter',
-    component: () => import(/* webpackChunkName: "user" */ '../views/UserCenter.vue'),
+    component: () => import(/* webpackChunkName: "user" */ '../views/user/UserCenter.vue'),
     meta: { requiresAuth: true }
   },
   {
